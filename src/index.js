683a9db12de62bac9e74a2218c04dad7c4f81fc4
dotAugis <Strigas.Augustinas@gmail.com>
2023-04-14 10:10:23 +0200

    Removing App & adding StartMain

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import StartMain from './StartMain';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <h1>Hello world</h1>
-    <App />
+    <StartMain />
   </React.StrictMode>
 );
 
