c1addcfd9493208007f49294f15df291f6d7b56e
dotAugis <Strigas.Augustinas@gmail.com>
2023-04-14 10:25:24 +0200

    Minor progress update

---
+++ src/StartMain.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './StartMain.css';
+
+class StartMain extends Component {
+    render() {
+        return (
+        <div>
+        <h1>Testing</h1>
+        <p>Return unreachable code</p>
+        </div>
+        );
+    }
+}
+
+export default StartMain;
