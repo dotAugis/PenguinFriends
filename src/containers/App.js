830ebe38d22d2622c1392129d95250c1c9a36f44
dotAugis <Strigas.Augustinas@gmail.com>
2023-04-28 11:10:49 +0200

    Finishing, cleaning up, organizing and building.

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      friends: [],
+      searchfield: '',
+    }
+  }
+
+  componentDidMount() { // user API
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => this.setState({ friends: users}));
+  }
+
+    onSearchChange = (event) => { 
+      this.setState({ searchfield: event.target.value })
+    }
+
+  render() {
+    const { friends, searchfield } = this.state;
+    const filteredFriends = friends.filter(friend => {
+      return friend.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+      return !friends.length ?  // delayed load behavior
+      <h1 class='loading' className = 'tc'><span>Loading</span> friendlist<span>!</span></h1> :    
+      (
+      // normal load behavior with scroll
+        <div className='tc'>
+          <h1 class='title'><span>social</span>Friends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>  
+            <CardList friends={filteredFriends}/> 
+          </Scroll>
+        </div>
+      );
+  }
+}
+
+export default App;
